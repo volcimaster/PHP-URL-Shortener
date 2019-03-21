ccf8ff4ab8457c92085977bb44ec47d7980b97ff
volcimaster <volcimaster@gmail.com>
2019-03-21 19:36:05 -0400

    Update shortenedurls.sql

--- shortenedurls.sql
+++ shortenedurls.sql
@@ -4,10 +4,9 @@
 
 CREATE TABLE `shurl` (
   `id` int(10) unsigned NOT NULL auto_increment,
-  `short` varchar(33) NOT NULL UNIQUE,
-  `long_url` varchar(768) NOT NULL UNIQUE,
+  `long_url` varchar(2550) NOT NULL,
   `created` int(10) unsigned NOT NULL,
   `creator` char(15) NOT NULL,
   `referrals` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
